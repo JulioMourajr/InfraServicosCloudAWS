ebe1ec72b856ee8f69f48df48e25705161884de4
Julio Moura <juliomourajr@hotmail.com>
2024-12-07 01:45:54 -0300

    :recycle: refactor: Mudando o nome do Bucket.

--- s3.py
+++ s3.py
@@ -5,7 +5,7 @@ sessao = boto3.Session(profile_name='default')
 client_s3 = sessao.client('s3')
 
 response = client_s3.create_bucket(
-    Bucket='bucket-da-empresa-ada-de-contabilidade-118421133023',
+    Bucket='bucket-da-empresa-ada-de-contabilidade-164330470722',
 )
 
 print("O bucket foi criado com sucesso!")
